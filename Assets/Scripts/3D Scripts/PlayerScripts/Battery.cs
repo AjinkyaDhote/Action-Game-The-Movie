d90641fd015db627925525888be238f1a590c26b
AjinkyDhote <ajinkya65@gmail.com>
2016-04-24 14:43:49 -0600

    Vikram's Changes. Fixed the battery and the ammo bug.

--- Assets/Scripts/3D Scripts/PlayerScripts/Battery.cs
+++ Assets/Scripts/3D Scripts/PlayerScripts/Battery.cs
@@ -2,34 +2,31 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Battery : MonoBehaviour {
+public class Battery : MonoBehaviour
+{
 
-    Text BatteryText;
-	private int batteryCount;
-    PlayerShooting playerShootingScript;
-    string batteryString;
-    void Start () {
+	private int batteryCount = 100;
+	PlayerShooting playerShootingScript;
+	string batteryString;
+	public int batteryPickedUp;
 
-		batteryCount = 100;
-        playerShootingScript = transform.GetChild(0).GetChild(0).GetComponent<PlayerShooting>();
-		BatteryText = transform.FindChild("Main Camera").transform.FindChild("Gun Camera").transform.FindChild("FPS UI Canvas").FindChild("BatteryText").GetComponent<Text>();
-        batteryString = " " + batteryCount;
-        BatteryText.text = batteryString;
-        BatteryText.color = Color.blue;
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Battery")
-        {
-            batteryCount += 10;
-            batteryString = "" + batteryCount;
-            BatteryText.text = batteryString;
-            Destroy(other.gameObject);
-        }
-        else if (other.tag == "Ammo")
-        {
-            playerShootingScript.PickupAmmo();
-            Destroy(other.gameObject);
-        }
-    }
+	void Start()
+	{
+		batteryPickedUp = 0;
+		playerShootingScript = transform.GetChild(0).GetChild(0).GetComponent<PlayerShooting>();
+		batteryString = " " + batteryCount;
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Battery")
+		{
+			batteryPickedUp += 50;
+			Destroy(other.gameObject);
+		}
+		else if (other.tag == "Ammo")
+		{
+			playerShootingScript.PickupAmmo();
+			Destroy(other.gameObject);
+		}
+	}
 }
