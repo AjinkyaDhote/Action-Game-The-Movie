1fd1f47ec8ec8e2db1fc8b3d0d6054c4c1bc30ad
AjinkyaDhote <ajinkya65@gmail.com>
2016-09-09 18:59:30 -0600

    Added new Payload Model and wrote Health Script for Payload.

---
+++ Assets/Scripts/3D Scripts/PayLoadScripts/PayLoadHealthScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PayLoadHealthScript : MonoBehaviour {
+
+    Text payLoadHealthText;
+    public float payLoadHealth;
+    string payLoadHealthString;
+
+	void Start ()
+    {
+        payLoadHealthText = GameObject.FindGameObjectWithTag("Player").transform.FindChild("Main Camera").transform.FindChild("Gun Camera").transform.FindChild("FPS UI Canvas").FindChild("PayLoadHealthText").GetComponent<Text>();
+        payLoadHealthString = " " + payLoadHealth;
+        payLoadHealthText.color = Color.white;
+        payLoadHealthText.text = payLoadHealthString;
+    }
+
+    public void PayLoadDamage(float damage)
+    {
+        payLoadHealth -= damage;
+        payLoadHealthString = "" + payLoadHealth;
+        payLoadHealthText.text = payLoadHealthString;
+
+        if(payLoadHealth <= 30)
+        {
+            payLoadHealthText.color = Color.red;
+        }
+
+        if(payLoadHealth <= 0)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            GameManager.Instance.win_Lose = false;
+            GameManager.Instance.win_Lose_Message = "Game Over";
+            GameManager.Instance.GoToWinLoseScene();
+        }
+    } 	    	
+}
