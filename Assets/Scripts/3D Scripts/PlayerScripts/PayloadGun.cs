76b425d047c31f40af999911005b2fbf409c2736
AjinkyaDhote <ajinkya65@gmail.com>
2016-10-06 12:31:57 -0600

    Tweaked the collision between Payload and Player

---
+++ Assets/Scripts/3D Scripts/PlayerScripts/PayloadGun.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PayloadGun : MonoBehaviour {
+
+    Transform payLoadtransform;
+    //Canvas canvas;
+
+	void Start ()
+    {
+        payLoadtransform = GameObject.FindGameObjectWithTag("PayLoad").GetComponent<Transform>(); 	
+
+	}
+	
+	void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "PayLoad")
+        {
+            Debug.Log("Collided with Payload");
+        }
+        
+    }
+}
