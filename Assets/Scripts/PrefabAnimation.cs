0ca5cf5068d014b1f3b86c1adee29236b3ca0379
yashsvir <yashsvir@gmail.com>
2016-10-23 21:41:29 -0600

    Rotation corrected.

--- Assets/Scripts/PrefabAnimation.cs
+++ Assets/Scripts/PrefabAnimation.cs
@@ -6,16 +6,18 @@ public class PrefabAnimation : MonoBehaviour {
     // Use this for initialization
     void Start () {
         pauseMenuScript = GameObject.FindWithTag("PauseMenu").GetComponent<PauseMenu>();
+        if (gameObject.tag == "Battery")
+            transform.rotation = Quaternion.Euler(0f, 0f, 90f);
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (!pauseMenuScript.isPaused)
         {
-            if (this.gameObject.tag == "Ammo")
-                this.transform.Rotate(0, 2f, 0);
-            if (this.gameObject.tag == "Battery")
-                this.transform.Rotate(0, 0, 2f);
+            if (gameObject.tag == "Ammo")
+                transform.Rotate(0, 2f, 0f);
+            if (gameObject.tag == "Battery")
+                transform.Rotate(transform.right, 2f, Space.World);
         }
     }
 }
