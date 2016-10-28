2766c575d0e8624be670141a791a0425f5f1605e
Ameya Gadkari <ameya.gadkari@utah.edu>
2016-10-28 08:40:00 -0600

    Reverted wasdMovement back to how it was

--- Assets/Scripts/3D Scripts/PlayerScripts/wasdMovement.cs
+++ Assets/Scripts/3D Scripts/PlayerScripts/wasdMovement.cs
@@ -5,8 +5,6 @@ public class wasdMovement : MonoBehaviour
     private const float MAX_VELOCITY = 25.0f;
     public float playerAcceleration;
     private Rigidbody playerRigidBody;
-    private float horizontal;
-    private float vertical;
 
     void Start()
     {
@@ -17,18 +15,12 @@ public class wasdMovement : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
         {
-            playerRigidBody.AddRelativeForce(-horizontal * playerAcceleration * Time.deltaTime, 0, -vertical * playerAcceleration * Time.deltaTime, ForceMode.VelocityChange);
             ResetVelocities();
         }
-        else
-        {
-            horizontal = Input.GetAxis("Horizontal");
-            vertical = Input.GetAxis("Vertical");
-            playerRigidBody.AddRelativeForce(horizontal * playerAcceleration * Time.deltaTime, 0, vertical * playerAcceleration * Time.deltaTime, ForceMode.VelocityChange);
-            playerRigidBody.velocity = Vector3.ClampMagnitude(playerRigidBody.velocity, MAX_VELOCITY);
-        }
-    }
+        playerRigidBody.AddRelativeForce(Input.GetAxis("Horizontal") * playerAcceleration * Time.deltaTime, 0, Input.GetAxis("Vertical") * playerAcceleration * Time.deltaTime, ForceMode.VelocityChange);
+        playerRigidBody.velocity = Vector3.ClampMagnitude(playerRigidBody.velocity, MAX_VELOCITY);
 
+    }
     void ResetVelocities()
     {
         if (playerRigidBody.velocity != Vector3.zero)
