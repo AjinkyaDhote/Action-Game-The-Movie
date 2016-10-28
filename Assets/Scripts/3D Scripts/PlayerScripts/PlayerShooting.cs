7ded7d69f43109c4de1f685b39589b867b4a3320
Ameya Gadkari <ameya.gadkari@utah.edu>
2016-10-28 08:33:28 -0600

    Added +10 animation

--- Assets/Scripts/3D Scripts/PlayerScripts/PlayerShooting.cs
+++ Assets/Scripts/3D Scripts/PlayerScripts/PlayerShooting.cs
@@ -35,8 +35,6 @@ public class PlayerShooting : MonoBehaviour
         }
     }
     private TimeSlow timeSlowScript;
-    //private GameObject laserPrefab;
-    //private GameObject laser;
     private AudioSource noBullets;
     [HideInInspector]
     public AudioSource currentGunAudio;
@@ -45,8 +43,6 @@ public class PlayerShooting : MonoBehaviour
     private WeaponSystem weaponSystemScript;
     private PauseMenu pauseMenuScript;
     private CountdownTimerScript countdownTimer;
-    //private EnemyHealth damageScript;
-    //private bool shooting = false;
 
     private RaycastHit hit;
     private int bulletCount = INITIAL_NUMBER_OF_BULLETS;
@@ -60,18 +56,13 @@ public class PlayerShooting : MonoBehaviour
     private Transform pistolBulletSpawnerTrasform;
 
     private Text AmmoText;
-    private GameObject AmmoAnimatePrefab;
-    private Vector3 initialPositionAmmoAnimate = new Vector3(-1.63f, -0.6f, 1.84f);
-    private List<GameObject> AmmoAnimateInstanceList = new List<GameObject>();
     private Text bulletOverText;
-    //private ParticleSystem wallHitParticleSystem;
-    //private AI_movement aiMovementScript;
     private GameObject wallHitPrefab;
+    private Animation AmmoAnimation;
 
     void Start()
     {
         timeSlowScript = transform.GetChild(2).FindChild("BulletTime").GetComponent<TimeSlow>();
-        //wallHitParticleSystem = GameObject.Find("WallHit").GetComponent<ParticleSystem>();
         wallHitPrefab = Resources.Load<GameObject>("WallHitPrefab/WallHit");
         shotgunBulletSpawnerTrasform = transform.GetChild(0).GetChild(0);
         pistolBulletSpawnerTrasform = transform.GetChild(1).GetChild(0);
@@ -84,32 +75,19 @@ public class PlayerShooting : MonoBehaviour
         }
         shotgunBulletRB = new Rigidbody[4];
         noBullets = GetComponent<AudioSource>();
-        //laserPrefab = Resources.Load("Laser Prefab/Laser") as GameObject;
         weaponSystemScript = GetComponent<WeaponSystem>();
         AmmoText = transform.FindChild("FPS UI Canvas").FindChild("AmmoText").GetComponent<Text>();
         AmmoText.text = " " + bulletCount;
         AmmoText.color = Color.white;
-        AmmoAnimatePrefab = Resources.Load<GameObject>("AmmoAnimatePrefab/AmmoAnimate");
         pauseMenuScript = GameObject.FindWithTag("PauseMenu").GetComponent<PauseMenu>();
-
         countdownTimer = GameObject.FindWithTag("InstructionsCanvas").transform.GetChild(0).GetComponent<CountdownTimerScript>();
         bulletOverText = transform.FindChild("FPS UI Canvas").FindChild("BulletOverText").GetComponent<Text>();
+        AmmoAnimation = transform.FindChild("FPS UI Canvas").FindChild("AmmoAnimateText").GetComponent<Animation>();
+        AmmoAnimation.gameObject.SetActive(false);
     }
     void Update()
-    {
-        foreach (GameObject AmmoInstance in AmmoAnimateInstanceList)
-        {
-            if (AmmoInstance.activeSelf && Time.timeScale != 0.0f)
-            {
-                AmmoInstance.transform.localPosition += new Vector3(0.0f, Time.deltaTime * (1.0f / Time.timeScale), 0.0f);
-                if (AmmoInstance.transform.localPosition.y > 1.38f)
-                {
-                    AddAmmo();
-                    AmmoInstance.SetActive(false);
-                }
-            }
-        }
-        
+    {  
+             
     }
     void FixedUpdate()
     {
@@ -209,134 +187,8 @@ public class PlayerShooting : MonoBehaviour
     {
         GameObject wallhit = Instantiate(wallHitPrefab, hitPoint + (WALL_HIT_PREFAB_POSITIONAL_OFFSET * hitNormal), Quaternion.LookRotation(hitNormal)) as GameObject;
         Destroy(wallhit, WALL_HIT_PREFAB_DESTROY_DELAY);
-        //wallHitParticleSystem.transform.position = hitPoint;
-        //wallHitParticleSystem.transform.rotation = Quaternion.LookRotation(hitNormal);
-        //wallHitParticleSystem.Play();
     }
 
-    /*void FixedUpdate()
-    {   
-        /*else
-        {
-            if (weaponSystemScript.currentWeaponInfo.gunName == "ShotGun")
-            {
-                if (bulletCount <= weaponSystemScript.currentWeaponInfo.ammoNeeded - 1)
-                {
-                    bulletOverText.text = "SWITCH GUN";
-                }
-                else if (bulletCount > weaponSystemScript.currentWeaponInfo.ammoNeeded - 1)
-                {
-                    bulletOverText.text = "";
-                }
-            }
-
-            else if (weaponSystemScript.currentWeaponInHand.Value.name == "Pistol")
-            {
-                if (bulletCount <= weaponSystemScript.currentWeaponInfo.ammoNeeded - 1)
-                {
-                    bulletOverText.text = "SWITCH GUN";
-                }
-                else if (bulletCount > weaponSystemScript.currentWeaponInfo.ammoNeeded - 1)
-                {
-                    bulletOverText.text = "";
-                }
-            }
-            else if (weaponSystemScript.currentWeaponInHand.Value.name == "MachineGun")
-            {
-                if (bulletCount > weaponSystemScript.currentWeaponInfo.ammoNeeded - 1)
-                {
-                    bulletOverText.text = "";
-                }
-            }
-        }
-
-        //if (shooting)// && (shotgunBulletCount > weaponSystemScript.currentWeaponInfo.ammoNeeded - 1))
-        //{
-            //weaponSystemScript.audioGun.Play();
-            ////muzzleFlash.Play();
-            //if (weaponSystemScript.currentWeaponInHand.Value.name == "ShotGun")
-            //{
-            //    //anim.SetTrigger("ShotGun");
-            //}
-            //else if (weaponSystemScript.currentWeaponInHand.Value.name == "PotatoGun" || weaponSystemScript.currentWeaponInHand.Value.name == "MachineGun")
-            //{
-            //    //anim.SetTrigger("Fire");
-            //}
-
-            //if (bulletCount > 0)
-            //{
-            //    bulletCount -= weaponSystemScript.currentWeaponInfo.ammoNeeded;
-            //    if (bulletCount <= 0)
-            //    {
-            //        bulletCount = 0;
-            //    }
-            //    AmmoText.text = " " + shotgunBulletCount;
-            //}
-            //if (bulletCount <= 10)
-            //{
-
-            //}
-            //shooting = false;
-            //RaycastHit hit;
-
-            //if (Physics.Raycast(transform.position, transform.forward, out hit, 50f))
-            //{
-            //    if (weaponSystemScript.currentWeaponInHand.Value.name == "GravityGun")
-            //    {
-            //        laser = Instantiate(laserPrefab);
-            //        laser.transform.SetParent(transform);
-            //        laser.transform.localPosition = new Vector3(0.695f, -0.809f, 2.805f);
-            //        laser.transform.localRotation = Quaternion.identity;
-            //        laser.GetComponent<Laser>().GetCorrectEnemy(hit.point);
-            //    }
-            //    if (hit.collider.tag == "Wall")
-            //    {
-            //        impacts[0].transform.position = hit.point;
-            //        Debug.Log(hit.point);
-            //        impacts[0].Play();
-            //    }
-            //    else if (hit.collider.tag == "BodyCollider")
-            //    {
-
-            //        impacts[1].transform.position = hit.point;
-            //        impacts[1].Play();
-            //        if(hit.collider.transform.CompareTag("SmallEnemy"))
-            //        {
-            //            aiMovementScript = hit.collider.transform.GetComponentInParent<AI_movement>();
-            //            if (!aiMovementScript.isPlayerSeenA)
-            //            {
-            //                aiMovementScript.Detection();
-            //            }
-            //        }
-            //        if(hit.collider.transform.parent.parent.CompareTag("BigEnemy"))
-            //        {
-            //            enemyThrowScript = hit.collider.transform.parent.parent.GetComponent<EnemyThrow>();
-            //            if (!enemyThrowScript.isPlayerSeen)
-            //            {
-            //                enemyThrowScript.Detection();
-            //            }
-            //        }               
-            //        damageScript = hit.collider.GetComponentInParent<EnemyHealth>();
-            //        if ((damageScript != null) && !damageScript.isKilled)
-            //        {
-            //            damageScript.Damage(weaponSystemScript.currentWeaponInfo.damageDealt);
-            //        }
-            //    }
-            //    else if ((hit.collider.tag == "HeadCollider"))
-            //    {
-            //        impacts[1].transform.position = hit.point;
-            //        impacts[1].Play();
-            //        damageScript = hit.collider.transform.GetComponentInParent<EnemyHealth>();
-            //        if ((damageScript != null) && (!damageScript.isKilled))
-            //        {
-            //            damageScript.Damage(25);
-            //            GameManager.Instance.headShots++;
-            //            Debug.Log("Head Shot");
-            //        }
-            //    }
-            //}
-        //}
-    }*/
     public void PickupAmmo()
     {
         int currentNumberOfBullets = bullets.Count;
@@ -346,15 +198,6 @@ public class PlayerShooting : MonoBehaviour
             bullets.Add(Instantiate(bulletPrefab));
             bullets[i].SetActive(false);
         }
-        GameObject AmmoAnimateInstance = Instantiate(AmmoAnimatePrefab, transform) as GameObject;
-        AmmoAnimateInstance.transform.LookAt(transform);
-        AmmoAnimateInstance.transform.localPosition = initialPositionAmmoAnimate;
-        AmmoAnimateInstance.SetActive(true);
-        AmmoAnimateInstanceList.Add(AmmoAnimateInstance);  
-    }
-
-    void AddAmmo()
-    {
         bulletCount += AMMO_PICK_UP;
         bulletOverText.text = "";
         AmmoText.text = bulletCount.ToString();
@@ -362,8 +205,9 @@ public class PlayerShooting : MonoBehaviour
         {
             AmmoText.color = Color.white;
         }
+        AmmoAnimation.gameObject.SetActive(true);
+        AmmoAnimation.Play();
     }
-
     Vector3 GenerateShotGunSpray(int i)
     {
         Quaternion rotation = Quaternion.AngleAxis(Random.Range(-SPARY_ANGLE, SPARY_ANGLE), ((i < 2) ? weaponSystemScript.currentWeaponInHand.Value.transform.forward : weaponSystemScript.currentWeaponInHand.Value.transform.up));
--- Assets/Scripts/3D Scripts/PlayerScripts/wasdMovement.cs
+++ Assets/Scripts/3D Scripts/PlayerScripts/wasdMovement.cs
@@ -4,32 +4,40 @@ public class wasdMovement : MonoBehaviour
 {
     private const float MAX_VELOCITY = 25.0f;
     public float playerAcceleration;
-    private Rigidbody rb;
+    private Rigidbody playerRigidBody;
+    private float horizontal;
+    private float vertical;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
         {
+            playerRigidBody.AddRelativeForce(-horizontal * playerAcceleration * Time.deltaTime, 0, -vertical * playerAcceleration * Time.deltaTime, ForceMode.VelocityChange);
             ResetVelocities();
         }
-        rb.AddRelativeForce(Input.GetAxis("Horizontal") * playerAcceleration * Time.deltaTime, 0, Input.GetAxis("Vertical") * playerAcceleration * Time.deltaTime, ForceMode.VelocityChange);
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, MAX_VELOCITY);
+        else
+        {
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+            playerRigidBody.AddRelativeForce(horizontal * playerAcceleration * Time.deltaTime, 0, vertical * playerAcceleration * Time.deltaTime, ForceMode.VelocityChange);
+            playerRigidBody.velocity = Vector3.ClampMagnitude(playerRigidBody.velocity, MAX_VELOCITY);
+        }
     }
 
     void ResetVelocities()
     {
-        if (rb.velocity != Vector3.zero)
+        if (playerRigidBody.velocity != Vector3.zero)
         {
-            rb.velocity = Vector3.zero;
+            playerRigidBody.velocity = Vector3.zero;
         }
-        if (rb.angularVelocity != Vector3.zero)
+        if (playerRigidBody.angularVelocity != Vector3.zero)
         {
-            rb.angularVelocity = Vector3.zero;
+            playerRigidBody.angularVelocity = Vector3.zero;
         }
     }
 }
