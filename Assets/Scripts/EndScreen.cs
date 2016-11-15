e9e2d7f1f17ea2592e0059cc5f09b9ed51aa4ab5
AjinkyaDhote <ajinkya65@gmail.com>
2016-11-15 12:36:30 -0700

    Added End State Door to the Win Trigger Prefab.

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScreen : MonoBehaviour {
+
+    Scoring Score;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Score = GetComponent<Scoring>();
+    }
+
+    public void ShowEndScreen()
+    {
+        Score.Score();
+        GameManager.Instance.win_Lose = true;
+        GameManager.Instance.win_Lose_Message = "Target Reached!";
+        GameManager.Instance.currentMenuState = GameManager.MenuState.SCORE_BOARD;
+        GameManager.Instance.GoToWinLoseScene();
+    }
+}
