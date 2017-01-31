69c37afbd28da5ece32e8fc7f347d91ad4be0ecf
AjinkyaDhote <ajinkya65@gmail.com>
2017-01-31 09:52:05 -0700

    No Changes

--- Assets/Scripts/3D Scripts/EnemyScripts/EnemySight.cs
+++ Assets/Scripts/3D Scripts/EnemyScripts/EnemySight.cs
@@ -6,7 +6,7 @@ public class EnemySight : MonoBehaviour
     public bool isPlayerSeen;
     private Camera enemyCamera;
     //RaycastHit[] hit;
-    float rayMaxDistance;
+    public float rayMaxDistance;
     //Ray[] ray;
     RaycastHit hit;
     Ray ray;
@@ -19,7 +19,7 @@ public class EnemySight : MonoBehaviour
         //ray = new Ray[8];
         //hit = new RaycastHit[4];
         rayCount = 4;
-        rayMaxDistance = 20;
+        //rayMaxDistance = 20;
 
         enemyCamera = GetComponentInChildren<Camera>();
         aiMovementScript = transform.GetComponentInParent<AI_movement>();
@@ -32,12 +32,12 @@ public class EnemySight : MonoBehaviour
         for (int i = 0; i < rayCount; i++)
         {
             ray = enemyCamera.ViewportPointToRay(new Vector3(i * (1 / (float)rayCount), 0.5f, 0));
-
+            Debug.DrawRay(ray.origin, ray.direction * (rayMaxDistance/*hit.distance*/), Color.red);
             if (Physics.Raycast(ray, out hit, rayMaxDistance))
             {
                 //Debug.Log(hit.collider.name);
 
-                Debug.DrawRay(ray.origin, ray.direction * (hit.distance),Color.red);
+                //Debug.DrawRay(ray.origin, ray.direction * (hit.distance),Color.red);
                 if ((hit.collider.tag == "Player") || (hit.collider.tag == "NewPayload"))
                 {
                     if (aiMovementScript != null)// && !aiMovementScript.isChasingPayload)
