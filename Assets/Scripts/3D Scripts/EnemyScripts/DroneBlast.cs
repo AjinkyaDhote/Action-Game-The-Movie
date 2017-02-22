42276f97217d1c6dbbfc5baf55d35521c318c03f
AjinkyaDhote <ajinkya65@gmail.com>
2017-02-21 19:30:31 -0700

    Made Changes Blast Prefab.

---
+++ Assets/Scripts/3D Scripts/EnemyScripts/DroneBlast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneBlast : MonoBehaviour
+{
+    private GameObject droneBlastPrefab;
+    private EnemyHealth enemyHealth;
+    private bool isBlastPlayed;
+	void Start ()
+    {
+        droneBlastPrefab = Resources.Load<GameObject>("DroneBlastPrefab/DroneBlast");
+        enemyHealth = GetComponent<EnemyHealth>();
+        isBlastPlayed = false;
+	}
+	
+	
+	void Update ()
+    {
+		if(enemyHealth.IsKilled && !isBlastPlayed)
+        {
+            PlayBlastEffect();
+        }
+	}
+
+    public void PlayBlastEffect()
+    {
+        isBlastPlayed = true;
+        GameObject blast = Instantiate(droneBlastPrefab, gameObject.transform.position, gameObject.transform.rotation) as GameObject;
+        Destroy(blast, 1f); 
+    }
+}
