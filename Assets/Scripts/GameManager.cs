935a8e2beef6c466e0131bc0e377e9c280d2940d
Suwas <suwas.walve@gmail.com>
2017-04-25 11:34:19 -0600

    Illustration edited...god mode false

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -56,7 +56,7 @@ public class GameManager : MonoBehaviour
     [HideInInspector]
     public float width2DPlane, width3DPlane, height2DPlane, height3DPlane;
 
-    private const bool _GOD_MODE = true;
+    private const bool _GOD_MODE = false;
 
     public bool GOD_MODE
     {
